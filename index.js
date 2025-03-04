971dba1f2b321940c00e112547f5490f7fd32bdb
tahmoor20 <tahmoor20000@gmail.com>
2025-03-04 21:59:08 +0000

    Second Comment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 dotenv.config();
 
 const app = express(); 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; //ssssss
 
 // Middleware
 app.use(cors());
